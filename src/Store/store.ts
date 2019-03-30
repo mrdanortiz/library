54fd325a5ebb18d4a9a210baa229a0d2b3902b3e
mrdanortiz <dortiz357@gmail.com>
2019-03-30 12:22:52 -0500

    add redux dev tool extension and deploy script

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,13 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { createEpicMiddleware } from "redux-observable";
 import { rootEpic } from "../Epics/rootEpics";
 import { rootReducer } from "../Reducers/rootReducers";
 
 const epicMiddleware = createEpicMiddleware();
-// @ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(epicMiddleware)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(epicMiddleware)));
 epicMiddleware.run(rootEpic);
 
 export default store;
